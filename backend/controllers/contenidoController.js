b52a0a26eb5c9d0d9cdbf51e6201d214fe59bc3d
ifernandeznv <ifernandeznv@gmail.com>
2022-11-23 20:13:26 -0600

    Creación de las rutas y controladores del content

---
+++ backend/controllers/contenidoController.js
@@ -0,0 +1,16 @@
+import cloudinary from 'cloudinary';
+import * as dotenv from 'dotenv';
+import Actividades from '../models/actividades.js';
+import Usuario from '../models/usuario.js';
+import axios from 'axios';
+
+dotenv.config();
+
+const obtenerContenido = async (req, res) => {
+    
+}
+
+
+export {
+    
+}
