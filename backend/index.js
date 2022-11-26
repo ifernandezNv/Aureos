c535b06e19bccbb154ea993df6e906e417126b7d
ifernandeznv <ifernandeznv@gmail.com>
2022-11-25 20:31:53 -0600

    Primeros pasos en la implementación de socket.io

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import conectarDB from './config/db.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import formularioRoutes from './routes/formularioRoutes.js';
 import actividadesRoutes from './routes/actividadesRoutes.js';
-
+import {Server} from 'socket.io';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,9 +18,15 @@ app.use(cors());
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/formulario', formularioRoutes);
 app.use('/api/actividades', actividadesRoutes);
-
-
-app.listen(4000, ()=>{
+const servidor = app.listen(4000, ()=>{
     console.log('Servidor conectado al puerto 4000');
+})
+
+const io = new Server(servidor, {
+    pingTimeout: 6000,
+});
+
+io.on('connection', (socket) => {
+    console.log('Conectado al servidor y socket.io está funcionando');
 })
 
