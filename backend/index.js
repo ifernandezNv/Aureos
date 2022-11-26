ef41d31ceb6870d93e2fbeb5d4b2345027ba87c0
ifernandeznv <ifernandeznv@gmail.com>
2022-11-26 17:37:30 -0600

    Se cancela la implementación de socket.io

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ import conectarDB from './config/db.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import formularioRoutes from './routes/formularioRoutes.js';
 import actividadesRoutes from './routes/actividadesRoutes.js';
-import {Server} from 'socket.io';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,18 +17,7 @@ app.use(cors());
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/formulario', formularioRoutes);
 app.use('/api/actividades', actividadesRoutes);
-const servidor = app.listen(4000, ()=>{
+
+app.listen(4000, ()=>{
     console.log('Servidor conectado al puerto 4000');
 })
-
-const io = new Server(servidor, {
-    pingTimeout: 6000,
-});
-
-io.on('connection', (socket) => {
-    console.log('Conectado al servidor y socket.io está funcionando');
-    socket.on('nueva actividad', (actividad)=>{
-        socket.to(actividad).emit('actividad agregada correctamente', actividad);
-    });
-})
-
