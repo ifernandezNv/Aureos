5e8aa497a0444530b2ddce61a19066c1f6db0b4a
ifernandeznv <ifernandeznv@gmail.com>
2022-11-26 17:21:02 -0600

    ajustes a la creación del evento de socket.io

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const io = new Server(servidor, {
 
 io.on('connection', (socket) => {
     console.log('Conectado al servidor y socket.io está funcionando');
-    io.on('nueva actividad', (actividad)=>{
+    socket.on('nueva actividad', (actividad)=>{
         socket.to(actividad).emit('actividad agregada correctamente', actividad);
     });
 })
