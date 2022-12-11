1796b4db9833c59a28144e6ef660bbc14031e897
ifernandeznv <ifernandeznv@gmail.com>
2022-12-11 12:21:19 -0600

    Ajustes generales

--- backend/controllers/actividadesController.js
+++ backend/controllers/actividadesController.js
@@ -2,7 +2,6 @@ import cloudinary from 'cloudinary';
 import * as dotenv from 'dotenv';
 import Actividades from '../models/actividades.js';
 import Usuario from '../models/usuario.js';
-import axios from 'axios';
 
 dotenv.config();
 
